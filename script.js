65bb8f183ac956b79cdf9e976e1828cf112e188a
Trista McCleary <trista_mccleary@yahoo.com>
2018-04-06 01:02:32 -0400

    connected js with html

--- script.js
+++ script.js
@@ -1,103 +1,150 @@
-var createPolitician = function(name, partyColor) {
-  var politician = {};
-  politician.name = name;
-  politician.electionResults = null;
-  politician.totalVotes = 0;
-  politician.partyColor = partyColor;
+// Assigning candidate names and party color
+var getCandidate = function(name, partyColor)
+{
+    var politician = {};
+    politician.name = name;
+    politician.electionResults = null;
+    politician.totalVotes = 0;
+    politician.partyColor = partyColor;
 
-  return politician;
+    return politician;
 };
-// assign a party color to each politician
-var jane = createPolitician("Jane Doe", [132, 17, 11]);
-var john = createPolitician("John Doe", [245, 141, 136]);
 
-var winner = "";
+// assign a party color to each politician
+var candidate1 = getCandidate("Jane Doe", [132, 17, 11]);
+var candidate2 = getCandidate("John Doe", [245, 141, 136]);
 
 // Election results by state
-jane.electionResults = [5, 1, 7, 2, 33,  6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
+candidate1.electionResults = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
+candidate2.electionResults = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3, 9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 3, 1, 2, 11, 2, 3, 1];
 
-john.electionResults = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3, 9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 3, 1, 2, 11, 2, 3, 1];
+// Changing some state results that were initially misreported
+candidate1.electionResults[9] = 1; //Florida
+candidate1.electionResults[4] = 17; //California
+candidate1.electionResults[43] = 11; //Texas
 
-// change state results that were initially miscounted
-jane.electionResults[9] = 1; //Florida
-jane.electionResults[4] = 17; //California
-jane.electionResults[43] = 11; //Texas
+candidate2.electionResults[9] = 28; //Florida
+candidate2.electionResults[4] = 38; //California
+candidate2.electionResults[43] = 27; //Texas
 
-john.electionResults[9] = 28; //Florida
-john.electionResults[4] = 38; //California
-john.electionResults[43] = 27; //Texas
+//console.log(candidate1.electionResults);
+//console.log(candidate2.electionResults);
 
-//console.log(jane.electionResults);
-//console.log(john.electionResults);
+// State results
+var setStateResults = function(state)
+{
+    theStates[state].winner = null;
 
-// State Results
-var setStateResults = function(state) {
-  theStates[state].winner = null;
+    if ( candidate1.electionResults[state] > candidate2.electionResults[state] )
+    {
+        theStates[state].winner = candidate1.name;
+        theStates[state].winnerColor = candidate1.partyColor;
+    }
+    else if ( candidate1.electionResults[state] < candidate2.electionResults[state] )
+    {
+        theStates[state].winner = candidate2.name;
+        theStates[state].winnerColor = candidate2.partyColor;
+    }
 
-  if(jane.electionResults[state] > john.electionResults[state]) {
-    theStates[state].winner = jane;
-  } else if (jane.electionResults[state] <
-    john.electionResults[state]) {
-      theStates[state].winner = john;
-  } else {
-    winner = draw;
-  }
+	// populate info in the bottom table for state name and abbrev
+	var stateInfoTable = document.getElementById('stateResults');
 
+	var header = stateInfoTable.children[0];
+	var body = stateInfoTable.children[1];
 
+	var stateName = header.children[0].children[0];
+	var abbrev = header.children[0].children[1];
 
-  var stateWinner = theStates[state].winner;
+	var candidate1Name = body.children[0].children[0];
+	var candidate2Name = body.children[1].children[0];
 
-    if(stateWinner !== null) {
-      theStates[state].rgbColor = stateWinner.partyColor;
-    } else {
-      theStates[state].rgbColor = RGB(11, 32, 570);
+	var candidate1Results = body.children[0].children[1];
+	var candidate2Results = body.children[1].children[1];
+
+	var winnersName = body.children[2].children[1];
+
+	stateName.innerText = theStates[state].nameFull;
+	abbrev.innerText = "(" +theStates[state].nameAbbrev + ")";
+
+	candidate1Name.innerText = candidate1.name;
+	candidate2Name.innerText = candidate2.name;
+
+	candidate1Results.innerText = candidate1.electionResults[state];
+	candidate2Results.innerText = candidate2.electionResults[state];
+
+	if (theStates[state].winner === null)
+	{
+	  winnersName.innerText = "DRAW";
+	}
+	else
+	{
+	  winnersName.innerText = theStates[state].winner;
+	}
+
+  // Set state with winner's color
+	var stateColor = theStates[state].winnerColor;
+
+    if (theStates[state].winner !== null)
+    {
+      theStates[state].rgbColor = stateColor;
+    }
+    else
+    {
+      theStates[state].rgbColor = [11,32,57];
     }
 };
 
-// information to popluate the table at the top of the page with each candidates name and total number of votes
-var countryInfoTable = document.getElementById("countryResults");
-var row = countryInfoTable.children[0].children[0];
-
-row.children[0].innerText = jane.name;
-row.children[1].innerText = jane.totalVotes;
-row.children[2].innerText = john.name;
-row.children[3].innerText = john.totalVotes;
-row.children[5].innerText = winner;
-
-// popluate info in the bottom table for state name and abbr
-var stateInfoTable = document.getElementById("stateResults");
-var header = stateInfoTable.children[0].children[0];
-var stateName = header.children[0];
-
-stateName.innerText = theStates[state].nameFull;
-
-
 // method to tally the votes for each politician
-politician.addTotalVotes = function() {
-  this.totalVotes = 0;
-  for (var i = 0; i < this.electionResults.length; i++) {
-    this.totalVotes = this.totalVotes + this.electionResults[i];
-  }
+candidate1.tallyVotes = function()
+{
+    this.totalVotes = 0;
+
+    for (var i = 0; i < this.electionResults.length; i++)
+    {
+         this.totalVotes = this.totalVotes + this.electionResults[i];
+         //console.log(this.totalVotes);
+    }
 };
+candidate1.tallyVotes();
 
-jane.addTotalVotes();
-john.addTotalVotes();
+candidate2.tallyVotes = function()
+{
+    this.totalVotes = 0;
 
-console.log(jane.totalVotes);
-console.log(john.totalVotes);
+    for (var i = 0; i < this.electionResults.length; i++)
+    {
+         this.totalVotes = this.totalVotes + this.electionResults[i];
+         //console.log(this.totalVotes);
+    }
+};
+candidate2.tallyVotes();
 
-// declares the winner
-if(jane.totalVotes > john.totalVotes) {
-  winner = jane.name;
-} else if (jane.totalVotes < john.totalVotes) {
-  winner = john.name;
-} else {
-  winner = "We have a tie!";
-}
+// Declare a winner
+var winner = function()
+{
+  if ( candidate1.totalVotes > candidate2.totalVotes )
+  {
+    winner = candidate1.name;
+  }
+  else if ( candidate2.totalVotes > candidate1.totalVotes )
+  {
+    winner = candidate2.name;
+  }
+  else
+    winner = "We have a tie!";
+};
 winner();
+// console.log("The winner is " + winner + "!!!");
 
-console.log("The winner is " + winner + "!!!");
 
 
-console.log("Jane's color is: " + jane.partyColor);
-console.log("John's color is: " + john.partyColor);
+// Populate table at top of page announcing the overall winner
+var countryInfoTable = document.getElementById('countryResults');
+
+var row = countryInfoTable.children[0].children[0];
+row.children[0].innerText = candidate1.name;
+row.children[1].innerText = candidate1.totalVotes;
+row.children[2].innerText = candidate2.name;
+row.children[3].innerText = candidate2.totalVotes;
+
+row.children[5].innerText = winner;
